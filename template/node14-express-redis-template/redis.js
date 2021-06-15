43090a19c92e8e5c3f0b970202b27bd0006b8ecc
Daniel Martínez Vigo <dmartinez@technisys.com>
2021-06-15 13:19:29 -0300

    build redis uri

--- template/node14-express-redis-template/redis.js
+++ template/node14-express-redis-template/redis.js
@@ -12,11 +12,17 @@ let _client_ok = false;
 
 let client;
 
+const uri = process.env.REDIS_URI
+  ? process.env.REDIS_URI
+  : `redis://${process.env.redis_username}:${process.env.redis_password}@${process.env.redis_endpoint}`;
+
+global.logger.info({ message: uri, label: 'REDIS URI' });
+
 if (process.env.REDIS_CERTIFICATE_BASE64) {
   global.logger.info({ message: 'module: redis', label: 'REDIS TLS' });
   const ca = Buffer.from(process.env.REDIS_CERTIFICATE_BASE64, 'base64').toString('utf-8');
   const tls = { ca };
-  client = redis.createClient(process.env.REDIS_URI, { tls });
+  client = redis.createClient(uri, { tls });
 
   client.on('connect', async () => {
     global.logger.info({ message: 'client connected', label: global.getLabel(__dirname, __filename) });
@@ -28,11 +34,11 @@ if (process.env.REDIS_CERTIFICATE_BASE64) {
     global.Raven.captureException(err);
     _client_ok = false;
     await sleep(1000);
-    client = redis.createClient(process.env.REDIS_URI, { tls });
+    client = redis.createClient(uri, { tls });
   });
 } else {
   const options = {
-    url: process.env.REDIS_URI,
+    url: uri,
   };
   client = redis.createClient(options);
 
